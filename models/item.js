68cedcbc2d8c623b46a4b62bf29928d2fec15ee6
Shiielty <56679623+Shiielty@users.noreply.github.com>
2023-12-27 18:05:14 +0700

    Setup project, create item and category models

---
+++ models/item.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true },
+  category: [{ type: Schema.Types.ObjectId, ref: "Category" }],
+  price: { type: Number, required: true },
+  inStock: { type: Number, required: true },
+});
+
+// Virtual for book's URL
+ItemSchema.virtual("url").get(function () {
+  return `/catalog/item/${this._id}`;
+});
+
+// Export model
+module.exports = mongoose.model("Item", ItemSchema);
